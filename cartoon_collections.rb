9e7df7614be809392d26643eeac662453f913296
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-20 17:08:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(call_array)
+  index = 0 
+  call_array.each_with_index do |dwarve,index|
+    puts "#{index + 1}.#{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(p_calls)
+  new_calls= []
+  p_calls.collect do |elements|
+    new_calls << elements.capitalize + "!"
+  end
+  new_calls
 end
 
 def long_planeteer_calls# code an argument here
